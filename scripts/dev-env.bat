ac56908e9c31ef93642255721b9fd1eb33d984c0
sumedh22kar <sumedh2420@gmail.com>
2026-07-20 14:54:35 +0530

    chore: configure config server foundation

---
+++ scripts/dev-env.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set JAVA_HOME=D:\Java 21
+set PATH=%JAVA_HOME%\bin;%PATH%
+
+echo =========================================
+echo QuizHub Development Environment
+echo Java Home: %JAVA_HOME%
+echo =========================================
+
+cmd
